5e8e8ed0db16ac71597f04ff153088451cf445b4
TAKANO Mitsuhiro <takano32@gmail.com>
2020-05-12 06:11:02 +0900

    positional arguments completion

--- st2-completion.bash
+++ st2-completion.bash
@@ -7,10 +7,19 @@ _st2 () {
   local prev=${COMP_WORDS[COMP_CWORD-1]}
 
   if [ $cword -eq 1 ]; then
-    COMPREPLY=( $(compgen -W "login key" -- "$cur") )
+    opts=""
+    opts="run action action-alias auth login whoami apikey execution"
+    opts="$opts inquiry key pack policy policy-type rule webhook"
+    opts="$opts timer runner sensor trace trigger trigger-instance"
+    opts="$opts rule-enforcement workflow service-registry role role-assignment"
+    COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
   elif [ $cword -eq 2 ]; then
-    if [ $prev = "login" ]; then
-      COMPREPLY=( $(compgen -W "`cut -d: -f1 /etc/passwd | grep -vE '^(#|_).*$'`" -- "$cur") )
+    if [ $prev = "run" ]; then
+      # ToDo
+      true
+    elif [ $prev = "login" ]; then
+      user_list=`cut -d: -f1 /etc/passwd | grep -vE '^(#|_).*$'`
+      COMPREPLY=( $(compgen -W "$user_list" -- "$cur") )
     elif [ $prev = "key" ]; then
       COMPREPLY=( $(compgen -W "list set delete" -- "$cur") )
     fi
